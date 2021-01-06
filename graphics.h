686778825061c9627e91d2df1e9517484e0ba391
Shamil <ishamil86@gmail.com>
2021-01-06 22:12:36 +0300

    header added

---
+++ graphics.h
@@ -0,0 +1,16 @@
+//
+// Created by Shamil on 06.01.2021.
+//
+
+#ifndef CUB3D_GRAPHICS_H
+#define CUB3D_GRAPHICS_H
+
+typedef struct	s_data {
+	void		*image;
+	void		*address;
+	int			bits_per_pixel;
+	int			line_length;
+	int			endian;
+};				t_data;
+
+#endif //CUB3D_GRAPHICS_H
