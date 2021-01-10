6ba9c2859ad96ce6f17ce37a27e60b611ae70254
mismene <ishamil86@gmail.com>
2021-01-10 14:06:10 +0300

    close window

--- main.c
+++ main.c
@@ -1,16 +1,10 @@
 #include "graphics.h"
 
-t_player	*player_place(char **map)
+void	key_press(int keycode, t_all *all)
 {
-	t_player	player;
-
-	player.x = 10;
-	player.y = 10;
-}
-
-
-void	key_press(t_all *all)
-{
+	printf("keycode: %d\n", keycode);
+	if (keycode == 53)
+		exit(0);
 	mlx_pixel_put(all->data->mlx, all->data->window, 10, 10, 0x00FF0000);
 	mlx_put_image_to_window(all->data->mlx, all->data->window, all->data->image, 0, 0);
 }
@@ -19,10 +13,14 @@ int main()
 {
 	t_data		data;
 	t_all		all;
+	t_player	player;
 
 	data.mlx = mlx_init();
 	data.window = mlx_new_window(data.mlx, 500, 500, "Shamil");
+	data.image = mlx_new_image(data.mlx, 500, 500);
+	data.address = mlx_get_data_addr(data.image, &data.bits_per_pixel, &data.line_length, &data.endian);
 	all.data = &data;
-	mlx_hook(data.window, 2, (1L<<0), key_press, &all);
+	all.player = &player;
+	mlx_hook(data.window, 2, (1L<<0), &key_press, &all);
 	mlx_loop(data.mlx);
 }
