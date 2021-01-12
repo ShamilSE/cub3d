58dafeaa04de5755c2eae24c2b3e42705b1166c9
mismene <ishamil86@gmail.com>
2021-01-12 21:13:38 +0300

    commit before go home

--- draw_map.c
+++ draw_map.c
@@ -1,4 +1,27 @@
-void	draw_map(char **map)
+#include "headers/graphics.h"
+
+void	draw_map(char **map, t_all *all)
 {
-	
+	int i;
+	int j;
+	int x;
+	int y;
+
+	i = 0;
+	j = 0;
+	x = 10;
+	y = 10;
+	while (map[i])
+	{
+		while (map[i][j]) {
+			if (map[i][j] == '1') {
+				my_mlx_pixel_put(all->data, x, y, 0xFF0000);
+			}
+			j++;
+			x++;
+		}
+		y++;
+		i++;
+	}
+	mlx_put_image_to_window(all->data->mlx, all->data->window, all->data->image, 0, 0);
 }
