90815b08b3ccf73b264b2c32a3bb6c08d0945d7f
mismene <ishamil86@gmail.com>
2021-01-20 15:44:09 +0300

    got file pathes

--- config_file_handler/error_handle.c
+++ config_file_handler/error_handle.c
@@ -17,41 +17,69 @@ int		name_checker(char *name, char *chars)
 	return (0);
 }
 
-//void	get_resolution(){}
-
-void	get_config_info(char *line, int option)
+void	get_resolution(char *line)
 {
-	config_info = malloc(sizeof(t_config));
-	config_info->width = 0;
-	config_info->height = 0;
+	line++;
+	while (*line == ' ')
+		line++;
 	while (ft_isdigit(*line))
 	{
 		config_info->width = (config_info->width * 10) + (*line - 48);
 		line++;
 	}
+	while (*line == ' ')
+		line++;
+	while (ft_isdigit(*line))
+	{
+		config_info->height = (config_info->height * 10) + (*line - 48);
+		line++;
+	}
+}
+
+void	get_color(char *line)
+{
+	ft_printf("line with color: %s\n", line);
+}
+
+void	get_filepath(char *line)
+{
+	ft_printf("filepath line: %s\n", line);
+	char	c;
+	char	c2;
+
+	c = *line;
+	line++;
+	c2 = *line;
+	while (*line == ' ')
+		line++;
+	if (c == 'N')
+		config_info->north = ft_strdup(line);
+	else if (c == 'S' && c2 == 'O')
+		config_info->south = ft_strdup(line);
+	else if (c == 'S')
+		config_info->sprite = ft_strdup(line);
+	else if (c == 'W')
+		config_info->west = ft_strdup(line);
+	else if (c == 'E')
+		config_info->east =ft_strdup(line);
 }
 
 void	first_char(char *line)
 {
-	int		option;
-
+	config_info = malloc(sizeof(t_config));
+	config_info->width = 0;
+	config_info->height = 0;
 	if (*line == ' ')
 	{
 		ft_printf("Error\nno spaces at the start of a line!\n");
 		exit(1);
 	}
 	if (*line == 'R')
-		option = 1;
-	else if (*line == 'F')
-		option = 2;
-	else if (*line == 'S' && *line + 1 != 'O')
-		option = 3;
+		get_resolution(line);
+	else if (*line == 'F' || *line == 'C')
+		get_color(line);
 	else if (*line == 'N' || *line == 'S' || *line == 'W' || *line == 'E')
-		option = 4;
-	line++;
-	while (*line == ' ')
-		line++;
-	get_config_info(line, option);
+		get_filepath(line);
 }
 
 void	error_handle(char *config)
